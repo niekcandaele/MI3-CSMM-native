f962a019709cea2ee47182a07e55ceb21c8f7636
Niek Candaele <niekcandaele@gmail.com>
2018-08-26 13:33:45 +0200

    � Init

---
+++ app/src/main/java/com/ndaele/csmm/a7dtdservermanager/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.ndaele.csmm.a7dtdservermanager;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
